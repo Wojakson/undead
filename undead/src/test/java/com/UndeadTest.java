44c13b203b91b2172b65dae033374d1ca8afabdc
Wojakson <wanttie@gmail.com>
2018-06-16 15:34:35 +0200

    pewnie bedzie assertionError

--- undead/src/test/java/com/UndeadTest.java
+++ undead/src/test/java/com/UndeadTest.java
@@ -20,84 +20,84 @@ package com;
 
     public UndeadRepository undeadRepository;
 
-//    @Before
-//    public void initRepository(){
-//        undeadRepository = UndeadRepositoryFactory.getInstance();
-//        Undead szwedacz = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        Undead ghoul = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        Undead ozywieniec = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        Undead lisz = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//
-//        szwedacz.setId(1);
-//        szwedacz.setTyp("Szwedacz");
-//
-//        ghoul.setId(2);
-//        ghoul.setTyp("Ghoul");
-//
-//        ozywieniec.setId(3);
-//        ozywieniec.setTyp("Ozywieniec");
-//
-//        lisz.setId(4);
-//        lisz.setTyp("Lisz");
-//
-//        undeadRepository.addUndead(szwedacz);
-//        undeadRepository.addUndead(ghoul);
-//        undeadRepository.addUndead(ozywieniec);
-//        undeadRepository.addUndead(lisz);
-//    }
+    @Before
+    public void initRepository(){
+        undeadRepository = UndeadRepositoryFactory.getInstance();
+        Undead szwedacz = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        Undead ghoul = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        Undead ozywieniec = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        Undead lisz = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
 
-//    @Test
-//    public void getById()  throws Exception {
-//
-//        Undead zombie = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        zombie.setId(10);
-//        zombie.setTyp("Zgnilec");
-//        undeadRepository.addUndead(zombie);
-//        assertNotNull(undeadRepository.getById(zombie.getId()));
-//    }
-//
-//    @Test
-//    public void addUndead() throws Exception {
-//        Undead zombie = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        zombie.setId(10);
-//        zombie.setTyp("Zgnilec");
-//        undeadRepository.addUndead(zombie);
-//        assertNotNull(undeadRepository.getById(zombie.getId()));
-//        undeadRepository.deleteUndead(zombie);
-//
-//    }
-//
-//    @Test
-//    public void deleteUndead()  throws Exception {
-//        Undead zombie = undeadRepository.getById(1);
-//        undeadRepository.deleteUndead(zombie);
-//        if (undeadRepository.getAll().size() > 0){
-//            assertNotNull(undeadRepository.getAll());
-//        }else {
-//            assertNull(undeadRepository.getById(zombie.getId()));
-//        }
-//    }
-//
-//    @Test
-//    public void updateUndead ()  throws Exception {
-//        Undead wywloka = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
-//        wywloka.setTyp("wywloka");
-//        int zombieToUpdate = 1;
-//        undeadRepository.updateUndead(1, wywloka);
-//        assertEquals(1,undeadRepository.getById(zombieToUpdate).getId());
-//
-//        for(Undead undead : undeadRepository.getAll()){
-//            if(wywloka.getId() == zombieToUpdate) {
-//                //if(wywloka.getId().equals((long)zombieToUpdate)) {
-//                assertNotEquals(undead.getTyp(), wywloka.getTyp());
-//            }
-//        }
-//    }
-//
-//    @Test
-//    public void getAll() throws Exception {
-//        assertNotNull(undeadRepository.getAll());
-//    }
-//
-//
+        szwedacz.setId(1);
+        szwedacz.setTyp("Szwedacz");
+
+        ghoul.setId(2);
+        ghoul.setTyp("Ghoul");
+
+        ozywieniec.setId(3);
+        ozywieniec.setTyp("Ozywieniec");
+
+        lisz.setId(4);
+        lisz.setTyp("Lisz");
+
+        undeadRepository.addUndead(szwedacz);
+        undeadRepository.addUndead(ghoul);
+        undeadRepository.addUndead(ozywieniec);
+        undeadRepository.addUndead(lisz);
+    }
+
+    @Test
+    public void getById()  throws Exception {
+
+        Undead zombie = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        zombie.setId(10);
+        zombie.setTyp("Zgnilec");
+        undeadRepository.addUndead(zombie);
+        assertNotNull(undeadRepository.getById(zombie.getId()));
+    }
+
+    @Test
+    public void addUndead() throws Exception {
+        Undead zombie = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        zombie.setId(10);
+        zombie.setTyp("Zgnilec");
+        undeadRepository.addUndead(zombie);
+        assertNotNull(undeadRepository.getById(zombie.getId()));
+        undeadRepository.deleteUndead(zombie);
+
+    }
+
+    @Test
+    public void deleteUndead()  throws Exception {
+        Undead zombie = undeadRepository.getById(1);
+        undeadRepository.deleteUndead(zombie);
+        if (undeadRepository.getAll().size() > 0){
+            assertNotNull(undeadRepository.getAll());
+        }else {
+            assertNull(undeadRepository.getById(zombie.getId()));
+        }
+    }
+
+    @Test
+    public void updateUndead ()  throws Exception {
+        Undead wywloka = new Undead("Szkielet Wojownik","Szkielet", 5,"Cmentarz", "Demoralizacja");
+        wywloka.setTyp("wywloka");
+        int zombieToUpdate = 1;
+        undeadRepository.updateUndead(1, wywloka);
+        assertEquals(1,undeadRepository.getById(zombieToUpdate).getId());
+
+        for(Undead undead : undeadRepository.getAll()){
+            if(wywloka.getId() == zombieToUpdate) {
+                //if(wywloka.getId().equals((long)zombieToUpdate)) {
+                assertNotEquals(undead.getTyp(), wywloka.getTyp());
+            }
+        }
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        assertNotNull(undeadRepository.getAll());
+    }
+
+
 }
